77deb83b699d19dd848239874ee585f0237c192c
Prabhat-Kumar-Ahirwar <prabhatmmg007@gmail.com>
2026-05-11 18:38:50 +0530

    initial commit

---
+++ src/main/java/com/example/BankTransactionSimulator/BankTransactionSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.BankTransactionSimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTransactionSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankTransactionSimulatorApplication.class, args);
+	}
+
+}
